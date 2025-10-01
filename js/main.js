4b8cdb03a40e30f6cf463c781e8b3e523bc7e0fb
Bloxmine <heindijstelbloem@hotmail.com>
2025-10-01 10:49:59 +0200

    added every combination of cocktail

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ const selectedIngredientsDiv = document.getElementById('selectedIngredients');
 const result = document.getElementById('result');
 const resetButton = document.getElementById('resetButton');
 
+const music = new Audio('music/johnsonrag.mp3');
+music.loop = true;
+music.volume = 0.5; // Set a comfortable volume level
+
 let currentStep = 1; // 1 = base, 2 = mixer, 3 = garnish
+let musicPlaying = false;
 let selectedIngredients = [];
 const selections = {
     base: null,
@@ -14,27 +19,186 @@ const selections = {
 };
 
 const cocktails = {
-    "Vodka + Orange Juice + Cherry": "You made a Screwdriver! 🍹",
-    "Gin + Tonic Water + Lime": "You made a Gin and Tonic! 🍸",
-    "Rum + Cola + Lime": "You made a Cuba Libre! 🥃",
-    "Tequila + Orange Juice + Lime": "You made a Tequila Sunrise! 🌅",
-    "Whiskey + Cola + Lemon": "You made a Whiskey Cola! 🥃",
-    "Vodka + Cranberry Juice + Lime": "You made a Cape Codder! 🍸",
-    "Gin + Soda Water + Mint": "You made a Gin Fizz! 🍸",
-    "Rum + Orange Juice + Cherry": "You made a Mai Tai! 🍹",
-    "Tequila + Soda Water + Lime": "You made a Paloma! 🍹",
-    "Whiskey + Tonic Water + Lemon": "You made a Whiskey Tonic! 🥃",
-    "Vodka + Cola + Lemon": "You made a Vodka Cola! 🍹",
-    "Gin + Cranberry Juice + Lime": "You made a Cranberry Gin! 🍸",
-    "Rum + Tonic Water + Lime": "You made a Rum Tonic! 🍹",
-    "Tequila + Cranberry Juice + Lime": "You made a Tequila Cranberry! 🍹",
-    "Whiskey + Orange Juice + Cherry": "You made a Whiskey Sour! 🥃",
-    "Vodka + Vermouth + Olive": "You made a Dirty Martini! 🍸",
-    "Gin + Vermouth + Olive": "You made a Classic Martini! 🍸",
-    "Rum + Ginger Ale + Lime": "You made a Dark 'n' Stormy! 🍹",
-    "Tequila + Ginger Ale + Lime": "You made a Mexican Mule! 🍹",
-    "Whiskey + Ginger Ale + Lemon": "You made a Whiskey Ginger! 🥃",
-    "Vodka + Gin + Rum + Tequila + Whiskey + Tonic Water + Cola + Orange Juice + Cranberry Juice + Soda Water + Lemon + Lime + Mint + Cherry + Olive": "Ewwwww... what the hell? 🍹"
+    "Vodka + Tonic Water + Soda Water": "You made a The Clarity! ✨",
+    "Vodka + Tonic Water + Lemon": "You made a Vodka Tonic with Lemon! 🍋",
+    "Vodka + Tonic Water + Lime": "You made a Vodka Tonic! 🧊",
+    "Vodka + Tonic Water + Mint": "You made a Verdant Sparkler! 🍸",
+    "Vodka + Tonic Water + Cherry": "You made a Crimson Fizz! 🍒",
+    "Vodka + Tonic Water + Olive": "You made a The Martini Wannabe! 🍸",
+    "Vodka + Cola + Soda Water": "You made a Cola Cloud! 🍸",
+    "Vodka + Cola + Lemon": "You made a Sparkling Lemon Spirit! 🍸",
+    "Vodka + Cola + Lime": "You made a Vodka Libre! 🍹",
+    "Vodka + Cola + Mint": "You made a Minty Cola Rush! 🍃",
+    "Vodka + Cola + Cherry": "You made a Cherry Cola Crush! 🍒",
+    "Vodka + Cola + Olive": "You made a The Olive's Lament! 🤔",
+    "Vodka + Ginger Ale + Soda Water": "You made a Crystal Ginger Spritz! 🥂",
+    "Vodka + Ginger Ale + Lemon": "You made a Zesty Ginger Fizz! 🍋",
+    "Vodka + Ginger Ale + Lime": "You made a Moscow Mule! 🍹",
+    "Vodka + Ginger Ale + Mint": "You made a Emerald Fizz! 🌿",
+    "Vodka + Ginger Ale + Cherry": "You made a Crimson Sparkler! 🍸",
+    "Vodka + Ginger Ale + Olive": "You made a The Dirty Mule! 🫒",
+    "Vodka + Vermouth + Soda Water": "You made a Vermouth Spritzer! 🥂",
+    "Vodka + Vermouth + Lemon": "You made a Vodka Martini (Lemon Twist)! 🍸",
+    "Vodka + Vermouth + Lime": "You made a The Verdant Sip! 🍸",
+    "Vodka + Vermouth + Mint": "You made a The Emerald Whisper! 🍸",
+    "Vodka + Vermouth + Cherry": "You made a The Ruby Reverie! 🍸",
+    "Vodka + Vermouth + Olive": "You made a Vodka Martini! 🍸",
+    "Vodka + Orange Juice + Soda Water": "You made a Sun-Kissed Fizz! 🍹",
+    "Vodka + Orange Juice + Lemon": "You made a Sunrise Zest! 🍊",
+    "Vodka + Orange Juice + Lime": "You made a Sunrise Zinger! ☀️",
+    "Vodka + Orange Juice + Mint": "You made a Emerald Dawn! 🍹🍃",
+    "Vodka + Orange Juice + Cherry": "You made a Cherry Sunrise! 🌅",
+    "Vodka + Orange Juice + Olive": "You made a The Salty Sunrise! 🫒",
+    "Vodka + Cranberry Juice + Soda Water": "You made a Sparkling Cape Codder! 🍒",
+    "Vodka + Cranberry Juice + Lemon": "You made a Cape Codder! 🍹",
+    "Vodka + Cranberry Juice + Lime": "You made a Cape Codder! 🍹",
+    "Vodka + Cranberry Juice + Mint": "You made a Ruby Refresher! 🍹",
+    "Vodka + Cranberry Juice + Cherry": "You made a Crimson Cherry Kiss! 🍒",
+    "Vodka + Cranberry Juice + Olive": "You made a The Crimson Brine! 🥴",
+    "Gin + Tonic Water + Soda Water": "You made a Gin Sonic! ✨",
+    "Gin + Tonic Water + Lemon": "You made a Gin & Tonic (with Lemon)! 🍋",
+    "Gin + Tonic Water + Lime": "You made a Gin & Tonic! 🍋",
+    "Gin + Tonic Water + Mint": "You made a Botanical Breeze! 🌿",
+    "Gin + Tonic Water + Cherry": "You made a Ruby Tonic! 🍸",
+    "Gin + Tonic Water + Olive": "You made a The Dirty Tonic! 🍸",
+    "Gin + Cola + Soda Water": "You made a The Gin-Cola Conundrum! 🍹",
+    "Gin + Cola + Lemon": "You made a The G&C Squeeze! 🍸",
+    "Gin + Cola + Lime": "You made a Gin & Cola Libre! 🍹",
+    "Gin + Cola + Mint": "You made a Verdant Gin Fizz! 🍹",
+    "Gin + Cola + Cherry": "You made a Ruby Gin Sparkler! 🍸",
+    "Gin + Cola + Olive": "You made a The Olive's Last Stand! 🍸",
+    "Gin + Ginger Ale + Soda Water": "You made a Gin Ginger Sparkler! 🫧",
+    "Gin + Ginger Ale + Lemon": "You made a Gin Ginger Snap! 🍸",
+    "Gin + Ginger Ale + Lime": "You made a Gin Buck! 🍸",
+    "Gin + Ginger Ale + Mint": "You made a Emerald Fizz! 🍸",
+    "Gin + Ginger Ale + Cherry": "You made a Ruby Fizz! 🍒",
+    "Gin + Ginger Ale + Olive": "You made a The Salty Fizz Fiasco! 🤨",
+    "Gin + Vermouth + Soda Water": "You made a The Botanical Spritz! 🥂",
+    "Gin + Vermouth + Lemon": "You made a The Zesty Botanist! 🌿",
+    "Gin + Vermouth + Lime": "You made a The Verdant Bloom! 🍸",
+    "Gin + Vermouth + Mint": "You made a Verdant Martini! 🍸",
+    "Gin + Vermouth + Cherry": "You made a Martinez! 🍸",
+    "Gin + Vermouth + Olive": "You made a Dry Martini! 🍸",
+    "Gin + Orange Juice + Soda Water": "You made a Orange Blossom Fizz! 🍊",
+    "Gin + Orange Juice + Lemon": "You made a Golden Gin Glow! ☀️",
+    "Gin + Orange Juice + Lime": "You made a Citrus Gin Bloom! 🍸",
+    "Gin + Orange Juice + Mint": "You made a Sun-Kissed Gin Garden! ☀️",
+    "Gin + Orange Juice + Cherry": "You made a Cherry Blossom Gin! 🌸",
+    "Gin + Orange Juice + Olive": "You made a The Garnish of Confusion! 🤨",
+    "Gin + Cranberry Juice + Soda Water": "You made a Crimson Sparkler! ✨",
+    "Gin + Cranberry Juice + Lemon": "You made a Crimson Zest! 🍸",
+    "Gin + Cranberry Juice + Lime": "You made a Crimson Gin Refresher! 🍸",
+    "Gin + Cranberry Juice + Mint": "You made a Crimson Bloom! 🌸",
+    "Gin + Cranberry Juice + Cherry": "You made a Crimson Gin Delight! 🍸",
+    "Gin + Cranberry Juice + Olive": "You made a Gin's Cranberry Conundrum! 🍸",
+    "Rum + Tonic Water + Soda Water": "You made a Tropical Tonic Spritz! 🍹",
+    "Rum + Tonic Water + Lemon": "You made a Island Breeze Tonic! 🏝️",
+    "Rum + Tonic Water + Lime": "You made a Island G&T (Rum & Tonic)! 🏝️",
+    "Rum + Tonic Water + Mint": "You made a The Tropic Tonic Tingle! 🌿",
+    "Rum + Tonic Water + Cherry": "You made a Crimson Current! 🍹",
+    "Rum + Tonic Water + Olive": "You made a The Briny Blunder! 🤢",
+    "Rum + Cola + Soda Water": "You made a Tropical Sparkler! 🍹",
+    "Rum + Cola + Lemon": "You made a Cuba Libre! 🇨🇺",
+    "Rum + Cola + Lime": "You made a Cuba Libre! 🇨🇺",
+    "Rum + Cola + Mint": "You made a Havana Mint Cooler! 🍸",
+    "Rum + Cola + Cherry": "You made a Havana Cherry! 🍒",
+    "Rum + Cola + Olive": "You made a Oliva Libre! 🫒",
+    "Rum + Ginger Ale + Soda Water": "You made a Ginger Rum Sparkler! 🍹",
+    "Rum + Ginger Ale + Lemon": "You made a Golden Ginger Zest! 🍹",
+    "Rum + Ginger Ale + Lime": "You made a Ginger Lime Rum Breeze! 🍹",
+    "Rum + Ginger Ale + Mint": "You made a The Minted Mariner! ⚓️",
+    "Rum + Ginger Ale + Cherry": "You made a The Cherry Rumbler! 🍒",
+    "Rum + Ginger Ale + Olive": "You made a Pirate's Paradox! 🫒",
+    "Rum + Vermouth + Soda Water": "You made a Island Aperitivo! 🍹",
+    "Rum + Vermouth + Lemon": "You made a The Vermouth Voyager! 🍹",
+    "Rum + Vermouth + Lime": "You made a The Compass Rose! 🍸",
+    "Rum + Vermouth + Mint": "You made a The Botanical Breeze! 🍃",
+    "Rum + Vermouth + Cherry": "You made a Scarlet Sailor! 🍒",
+    "Rum + Vermouth + Olive": "You made a The Brine & Barrel! 🍸",
+    "Rum + Orange Juice + Soda Water": "You made a Sun-Kissed Rum Spritzer! 🍹",
+    "Rum + Orange Juice + Lemon": "You made a Island Glow! 🍹",
+    "Rum + Orange Juice + Lime": "You made a Golden Horizon! 🍹",
+    "Rum + Orange Juice + Mint": "You made a Minted Sunrise! 🍹",
+    "Rum + Orange Juice + Cherry": "You made a Crimson Sunset! 🌅",
+    "Rum + Orange Juice + Olive": "You made a The Salty Seadog's Surprise! 🍸",
+    "Rum + Cranberry Juice + Soda Water": "You made a Crimson Rum Spritzer! 🥂",
+    "Rum + Cranberry Juice + Lemon": "You made a Crimson Refresher! 🍹",
+    "Rum + Cranberry Juice + Lime": "You made a The Cranberry Clipper! 🍹",
+    "Rum + Cranberry Juice + Mint": "You made a Ruby Rum Refresher! 🍹",
+    "Rum + Cranberry Juice + Cherry": "You made a Crimson Cherry Kiss! 🍹",
+    "Rum + Cranberry Juice + Olive": "You made a The Garnish Gone Rogue! 🍸",
+    "Tequila + Tonic Water + Soda Water": "You made a Agave Sparkler! 🥂",
+    "Tequila + Tonic Water + Lemon": "You made a Jalisco Lemon Fizz! 🥂",
+    "Tequila + Tonic Water + Lime": "You made a Tequila & Tonic! 🍋",
+    "Tequila + Tonic Water + Mint": "You made a Emerald Tonic! 🍸",
+    "Tequila + Tonic Water + Cherry": "You made a Crimson Cactus Cooler! 🍹",
+    "Tequila + Tonic Water + Olive": "You made a The Olive Branch Compromise! 🍸",
+    "Tequila + Cola + Soda Water": "You made a Agave Fizz! 🍹",
+    "Tequila + Cola + Lemon": "You made a Tequila Libre! 🍹",
+    "Tequila + Cola + Lime": "You made a Batanga! 🍹",
+    "Tequila + Cola + Mint": "You made a Emerald Sombrero! 🍹",
+    "Tequila + Cola + Cherry": "You made a Crimson Cola Kiss! 🍒",
+    "Tequila + Cola + Olive": "You made a The Brine Time Bomb! 💣",
+    "Tequila + Ginger Ale + Soda Water": "You made a Agave Ginger Fizz! 🍹",
+    "Tequila + Ginger Ale + Lemon": "You made a Jalisco Fizz! 🍸",
+    "Tequila + Ginger Ale + Lime": "You made a Agave Ginger Fizz! 🍹",
+    "Tequila + Ginger Ale + Mint": "You made a Agave Mint Sparkler! 🌿",
+    "Tequila + Ginger Ale + Cherry": "You made a Crimson Agave Fizz! 🍸",
+    "Tequila + Ginger Ale + Olive": "You made a The Salty Sombrero! 🍸",
+    "Tequila + Vermouth + Soda Water": "You made a Desert Bloom Spritz! 🍹",
+    "Tequila + Vermouth + Lemon": "You made a The Desert Bloom! 🌸",
+    "Tequila + Vermouth + Lime": "You made a The Desert Botanist! 🍸",
+    "Tequila + Vermouth + Mint": "You made a El Jardinero! 🌿",
+    "Tequila + Vermouth + Cherry": "You made a The Scarlet Serenade! 🍒",
+    "Tequila + Vermouth + Olive": "You made a The Dirty Desperado! 🍸",
+    "Tequila + Orange Juice + Soda Water": "You made a Orange Agave Fizz! 🍹",
+    "Tequila + Orange Juice + Lemon": "You made a Tequila Sunburst! ☀️",
+    "Tequila + Orange Juice + Lime": "You made a Agave Sunset! 🌅",
+    "Tequila + Orange Juice + Mint": "You made a Sunrise Mint Splash! 🍹",
+    "Tequila + Orange Juice + Cherry": "You made a Cherry Sunset! 🌅",
+    "Tequila + Orange Juice + Olive": "You made a Agave's Apology! 🍸",
+    "Tequila + Cranberry Juice + Soda Water": "You made a Crimson Sparkler! 🍒",
+    "Tequila + Cranberry Juice + Lemon": "You made a Aztec Sunset! 🌅",
+    "Tequila + Cranberry Juice + Lime": "You made a Cranberry Tequila! 🍹",
+    "Tequila + Cranberry Juice + Mint": "You made a Crimson Mint Agave Splash! 🍸",
+    "Tequila + Cranberry Juice + Cherry": "You made a Crimson Kiss! 🍒",
+    "Tequila + Cranberry Juice + Olive": "You made a The Agave Olive Twist! 🥴",
+    "Whiskey + Tonic Water + Soda Water": "You made a The Whiskey Spritz! 🥃",
+    "Whiskey + Tonic Water + Lemon": "You made a Amber Zest! 🍸",
+    "Whiskey + Tonic Water + Lime": "You made a The Zesty Whisk! 🍹",
+    "Whiskey + Tonic Water + Mint": "You made a Verdant Highball! 🌿",
+    "Whiskey + Tonic Water + Cherry": "You made a Cherrywood Sparkler! 🍸",
+    "Whiskey + Tonic Water + Olive": "You made a The Briny Buzzkill! 🍸",
+    "Whiskey + Cola + Soda Water": "You made a Whiskey Whisper Fizz! 🫧",
+    "Whiskey + Cola + Lemon": "You made a Whiskey & Cola with a Zest! 🥃",
+    "Whiskey + Cola + Lime": "You made a Whiskey Cola Refresher! 🥃",
+    "Whiskey + Cola + Mint": "You made a Emerald Cola Smash! 🍹",
+    "Whiskey + Cola + Cherry": "You made a Cherry Cola Whiskey! 🍒",
+    "Whiskey + Cola + Olive": "You made a The Regret! 🤢",
+    "Whiskey + Ginger Ale + Soda Water": "You made a Presbyterian! 🥃",
+    "Whiskey + Ginger Ale + Lemon": "You made a Whiskey Ginger Highball! 🥃",
+    "Whiskey + Ginger Ale + Lime": "You made a Whiskey Ginger! 🥃",
+    "Whiskey + Ginger Ale + Mint": "You made a Minted Whiskey Ginger! 🌿",
+    "Whiskey + Ginger Ale + Cherry": "You made a Cherry Whiskey Fizz! 🍒",
+    "Whiskey + Ginger Ale + Olive": "You made a The Olive Branch Compromise! 🍸",
+    "Whiskey + Vermouth + Soda Water": "You made a The Golden Hour Spritz! 🥂",
+    "Whiskey + Vermouth + Lemon": "You made a The Amber Zest! 🍸",
+    "Whiskey + Vermouth + Lime": "You made a The Botanical Brightener! 🍸",
+    "Whiskey + Vermouth + Mint": "You made a Emerald Elixir! 🍸",
+    "Whiskey + Vermouth + Cherry": "You made a Manhattan! 🥃",
+    "Whiskey + Vermouth + Olive": "You made a Dirty Manhattan! 🍸",
+    "Whiskey + Orange Juice + Soda Water": "You made a Golden Hour Fizz! 🍊✨",
+    "Whiskey + Orange Juice + Lemon": "You made a Golden Hour Grog! 🍹",
+    "Whiskey + Orange Juice + Lime": "You made a Sunset Whiskey Twist! 🥃",
+    "Whiskey + Orange Juice + Mint": "You made a Orange Whiskey Smash! 🥃",
+    "Whiskey + Orange Juice + Cherry": "You made a Orchard Ember! 🥃",
+    "Whiskey + Orange Juice + Olive": "You made a Whiskey Tango Foxtrot! 🍸",
+    "Whiskey + Cranberry Juice + Soda Water": "You made a Crimson Fizz! 🍸",
+    "Whiskey + Cranberry Juice + Lemon": "You made a Ruby Ember Sour! 🥃",
+    "Whiskey + Cranberry Juice + Lime": "You made a Ruby Rogue! 🍸",
+    "Whiskey + Cranberry Juice + Mint": "You made a Crimson Mint Smash! 🍹",
+    "Whiskey + Cranberry Juice + Cherry": "You made a Ruby Rebel! 🍸",
+    "Whiskey + Cranberry Juice + Olive": "You made a The Regret! 🍸"
 };
 
 ingredients.forEach(ingredient => {
@@ -44,13 +208,74 @@ ingredients.forEach(ingredient => {
 // reset button
 resetButton.addEventListener('click', resetSelection);
 
+// music toggle button
+const musicToggle = document.getElementById('musicToggle');
+musicToggle.addEventListener('click', toggleMusic);
+
+// attempt to start music automatically (will require user interaction due to browser policies)
+attemptAutoplay();
+
 // Get shaker element
 const cocktailShaker = document.getElementById('cocktailShaker');
 cocktailShaker.addEventListener('click', shakeCocktail);
 
+function toggleMusic() {
+    const musicToggle = document.getElementById('musicToggle');
+    
+    if (music.paused) {
+        music.play().then(() => {
+            musicPlaying = true;
+            musicToggle.textContent = '🔊';
+            musicToggle.title = 'Music On - Click to turn off';
+        }).catch(error => {
+            console.log('Music play failed:', error);
+            musicToggle.textContent = '🔇';
+            musicToggle.title = 'Click to play music';
+        });
+    } else {
+        music.pause();
+        musicPlaying = false;
+        musicToggle.textContent = '🎵';
+        musicToggle.title = 'Music Off - Click to turn on';
+    }
+}
+
+function attemptAutoplay() {
+    // Try to play music automatically
+    // This will likely fail due to browser autoplay policies, but we'll try
+    music.play().then(() => {
+        musicPlaying = true;
+        const musicToggle = document.getElementById('musicToggle');
+        musicToggle.textContent = '🔊';
+        musicToggle.title = 'Music On - Click to turn off';
+    }).catch(error => {
+        // Autoplay failed (expected), show music control for user to click
+        console.log('Autoplay prevented. User interaction required.');
+        const musicToggle = document.getElementById('musicToggle');
+        musicToggle.textContent = '🎵';
+        musicToggle.title = 'Click to play music';
+        
+        // Add a subtle pulse animation to draw attention to the music button
+        musicToggle.style.animation = 'pulse 2s infinite';
+    });
+}
+
 function handleIngredientClick(e) {
     const ingredient = e.target;
     const ingredientId = ingredient.id;
+    
+    // Start music on first user interaction if not already playing
+    if (!musicPlaying && music.paused) {
+        music.play().then(() => {
+            musicPlaying = true;
+            const musicToggle = document.getElementById('musicToggle');
+            musicToggle.textContent = '🔊';
+            musicToggle.title = 'Music On - Click to turn off';
+            musicToggle.style.animation = 'none'; // Remove pulse animation
+        }).catch(error => {
+            console.log('Music play failed on user interaction:', error);
+        });
+    }
     
     // check if ingredient is disabled
     if (ingredient.classList.contains('disabled')) {
@@ -193,17 +418,20 @@ function updateDisplay() {
 // helper to get ingredient name from id 
 function getIngredientName(id) {
     switch(id) {
+        // Bases
         case 'vodka': return 'Vodka';
         case 'gin': return 'Gin';
         case 'rum': return 'Rum';
         case 'tequila': return 'Tequila';
         case 'whiskey': return 'Whiskey';
+        // Mixers
         case 'tonic': return 'Tonic Water';
         case 'cola': return 'Cola';
         case 'gingerAle': return 'Ginger Ale';
         case 'vermouth': return 'Vermouth';
         case 'orangeJuice': return 'Orange Juice';
         case 'cranberryJuice': return 'Cranberry Juice';
+        // Garnishes
         case 'soda': return 'Soda Water';
         case 'lemon': return 'Lemon';
         case 'lime': return 'Lime';
